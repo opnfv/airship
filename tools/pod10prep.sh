916683fa5c90220db11ed36877bfb1b50b523ba2
Sridhar K. N. Rao <sridhar.rao@spirent.com>
2019-11-09 09:14:33 +0530

    Site: Intel Pod-10 Cleanup and Enable SRIOV
    This patch performs some cleanup and enable SRIOV for pod10.
    
    pod10prep.sh script is removed.
    Added environment file intel-pod10.env, which could be used by deploy.sh
    
    Signed-off-by: Sridhar K. N. Rao <sridhar.rao@spirent.com>
    Change-Id: I4c9759edf69511a426315a227012b5fbe9dea498

--- tools/pod10prep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-sed -i 's/ens785f1/eno4/g' ../type/cntt/software/charts/osh/openstack-compute-kit/neutron.yaml
-
-cp files/heat-public-net-deployment-pod10.yaml  files/heat-public-net-deployment.yaml
