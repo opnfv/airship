e0ed5132ec05e69d09d678506af985f7f873f1e0
James Gu <james.gu@att.com>
2020-11-19 20:34:59 -0800

    Functest configuration for AS 1.8
    added rally sanity blacklist, updated tempest conf and test.sh.
    
    Change-Id: I25b848e545a1a72428ba7d33ffebdd3f39be5b99
    Signed-off-by: James Gu <james.gu@att.com>

--- tools/test.sh
+++ tools/test.sh
@@ -9,6 +9,7 @@ export FUNCTEST_CACHE=${FUNCTEST_CACHE:-"${HOME}/.opnfv/functest"}
 export SITE=${2:-"intel-pod17"}
 
 cp tools/files/tempest_conf.yaml ${FUNCTEST_CACHE}
+cp tools/files/blacklist.yaml ${FUNCTEST_CACHE}
 TMP_DIR=$(mktemp -d)
 cd $TMP_DIR
 
@@ -18,10 +19,13 @@ trap "{ sudo rm -rf $TMP_DIR; }" EXIT
 cat > env << EOF
 EXTERNAL_NETWORK=public
 BLOCK_MIGRATION=False
+DEPLOY_SCENARIO=ovs
+NO_TENANT_NETWORK=true
+FLAVOR_EXTRA_SPECS=hw:mem_page_size:large
+NEW_USER_ROLE=_member_
 EOF
 
-cat > env_file << EOF
-export NEW_USER_ROLE=_member_
+cat > openstack.env << EOF
 export OS_AUTH_URL=http://identity-nc.$SITE.opnfv.org/v3
 export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_DOMAIN_NAME=default
@@ -51,10 +55,25 @@ run_tests() {
   sudo rm -rf ${FUNCTEST_CACHE}/results && mkdir ${FUNCTEST_CACHE}/results
 
   sudo docker run -it --env-file env --network host \
-      -v $(pwd)/env_file:/home/opnfv/functest/conf/env_file \
+      -v $(pwd)/openstack.env:/home/opnfv/functest/conf/env_file \
       -v ${FUNCTEST_CACHE}/images:/home/opnfv/functest/images \
       -v ${FUNCTEST_CACHE}/results:/home/opnfv/functest/results \
       -v ${FUNCTEST_CACHE}/tempest_conf.yaml:/usr/lib/python3.6/site-packages/functest/opnfv_tests/openstack/tempest/custom_tests/tempest_conf.yaml \
+      -v ${FUNCTEST_CACHE}/blacklist.yaml:/usr/lib/python3.6/site-packages/functest/opnfv_tests/openstack/rally/blacklist.yaml \
+      -v ${FUNCTEST_CACHE}/tempest_conf.yaml:/usr/lib/python2.7/site-packages/functest/opnfv_tests/openstack/tempest/custom_tests/tempest_conf.yaml \
+      -v ${FUNCTEST_CACHE}/blacklist.yaml:/usr/lib/python2.7/site-packages/functest/opnfv_tests/openstack/rally/blacklist.yaml \
+      -v /home/ubuntu/nc/functest/functest/core/tenantnetwork.py:/usr/lib/python3.6/site-packages/functest/core/tenantnetwork.py \
+      -v /home/ubuntu/nc/functest/functest/core/singlevm.py:/usr/lib/python3.6/site-packages/functest/core/singlevm.py \
+      -v /home/ubuntu/nc/functest/functest/ci/testcases.yaml:/usr/lib/python3.6/site-packages/functest/ci/testcases.yaml \
+      -v /home/ubuntu/nc/functest/functest/utils/env.py:/usr/lib/python3.6/site-packages/functest/utils/env.py \
+      -v /home/ubuntu/nc/functest/functest/opnfv_tests/openstack/api/connection_check.py:/usr/lib/python3.6/site-packages/functest/opnfv_tests/openstack/api/connection_check.py \
+      -v /home/ubuntu/nc/functest/functest/opnfv_tests/openstack/rally/scenario/sanity/opnfv-nova.yaml:/usr/lib/python3.6/site-packages/functest/opnfv_tests/openstack/rally/scenario/sanity/opnfv-nova.yaml \
+      -v /home/ubuntu/nc/functest/functest/core/tenantnetwork.py:/usr/lib/python2.7/site-packages/functest/core/tenantnetwork.py \
+      -v /home/ubuntu/nc/functest/functest/core/singlevm.py:/usr/lib/python2.7/site-packages/functest/core/singlevm.py \
+      -v /home/ubuntu/nc/functest/functest/ci/testcases.yaml:/usr/lib/python2.7/site-packages/functest/ci/testcases.yaml \
+      -v /home/ubuntu/nc/functest/functest/utils/env.py:/usr/lib/python2.7/site-packages/functest/utils/env.py \
+      -v /home/ubuntu/nc/functest/functest/opnfv_tests/openstack/api/connection_check.py:/usr/lib/python2.7/site-packages/functest/opnfv_tests/openstack/api/connection_check.py \
+      -v /home/ubuntu/nc/functest/functest/opnfv_tests/openstack/rally/scenario/sanity/opnfv-nova.yaml:/usr/lib/python2.7/site-packages/functest/opnfv_tests/openstack/rally/scenario/sanity/opnfv-nova.yaml \
       opnfv/functest-${1}:iruya bash
 }
 
